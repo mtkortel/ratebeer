59878768b1d9c7509dfad3487f40b4c4c6203ad6
Marko Kortelainen <marko.kortelainen@cs.helsinki.fi>
2016-02-15 19:01:42 +0200

    duplikaattien poisto

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160129174824) do
+ActiveRecord::Schema.define(version: 20160202101155) do
 
   create_table "beer_clubs", force: :cascade do |t|
     t.string   "name"
